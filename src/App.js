57d46cf41ce5159699fb6c8bda295c50d6f32c56
yaseressa <yaseressa222@gmail.com>
2022-05-07 12:59:57 +0300

    dep5

--- src/App.js
+++ src/App.js
@@ -197,7 +197,7 @@ useEffect(() => {
              <Header>
               <Link to='/' onClick={cage}><h1 id='hdh'>Somquest</h1></Link>
             <span id='men' onClick={hclickerc}>
-            <Link to='/'>
+            <Link to={`/categorizer/${category}`}>
               <span></span>
               <span></span>
               </Link>
